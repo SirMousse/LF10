f8f4104be0f8561df70c376ffc08289c0b250e8c
Vic <victoria.rehm@gfn.education>
2026-03-26 22:12:14 +0100

    Add files via upload

---
+++ Tag9/030_variablen/index.php
@@ -0,0 +1,13 @@
+<?php
+
+// Strings
+$name = "Mousse ";
+echo $name;
+
+$vorname = "Jacky";
+echo "\nHallo " . $vorname;
+
+// String verkettung
+$stadt = "Berlin";
+$bezirk = "Steglitz";
+echo "\nHallo " . $name . ", wohnst du auch in " . $bezirk . ", " . $stadt . "?";
